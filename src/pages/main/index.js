3aea983463645b13e6bfafcaa1086a80139ad4ca
Daniel Lucas <daniel.camargo@dokoro.com.br>
2019-11-09 10:45:02 -0300

    Listando produtos da API

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import './styles.css'
 import api from '../../services/api'
 
 export default class Main extends Component {
@@ -7,7 +8,9 @@ export default class Main extends Component {
         super(props)
 
         this.state = {
-            products: []
+            products: [],
+            page: 1,
+            productInfo: {}
         }
 
         this.loadProducts = this.loadProducts.bind(this)
@@ -18,18 +21,47 @@ export default class Main extends Component {
     }
 
     async loadProducts() {
-        const response = await api.get('/products')
-        this.setState({ products: response.data.docs })
+        const { page } = this.state
+        const response = await api.get(`/products?page=${page}`)
+        const { docs, ...productInfo } = response.data
+
+        this.setState({ products: docs, productInfo })
     }
 
+    renderProduct = product => (
+        <article key={ product._id }>
+            <strong>{ product.title }</strong>
+            <p>{ product.description }</p>
+
+            <a href="/#">Acessar</a>
+        </article>
+    )
+
     render() {
+        const { products, productInfo: { pages } } = this.state
+        let { page } = this.state
+
         return (
             <div className="product-list">
-            {
-                this.state.products.map(product => (
-                    <h2 key={ product._id }>{ product.title }</h2>
-                ))
-            }
+                { products.map(this.renderProduct) }
+
+                <div className="actions">
+                    <button
+                        disabled={ page === 1 }
+                        onClick={ () => this.setState({ page: --page }, this.loadProducts) }
+                    >
+                        Anterior
+                    </button>
+
+                    <span>{page}/{pages}</span>
+
+                    <button
+                        disabled={ page === pages }
+                        onClick={ () => this.setState({ page: ++page }, this.loadProducts) }
+                    >
+                        Próxima
+                    </button>
+                </div>
             </div>
         )
     }
